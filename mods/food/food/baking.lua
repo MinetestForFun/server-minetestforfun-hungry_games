ca86d52239294110310be9b5cc3686a43949b853
Obani <ObaniGemini@users.noreply.github.com>
2015-08-04 12:52:13 +0200

    Removed foods and crafts to the mod food

--- mods/food/food/baking.lua
+++ mods/food/food/baking.lua
@@ -21,24 +21,10 @@ minetest.register_craftitem(":farming:bread", {
 	on_use = minetest.item_eat(4),
 })
 
--- doughs
-minetest.register_craft({
-	output = '"food:bread_slice" 10',
-	recipe = {
-		{'"food:bread"'},
-	}
-})
-
 minetest.register_craftitem("food:bread_slice", {
 	description = "Bread Slice",
 	inventory_image = "food_bread_slice.png",
 	on_use = minetest.item_eat(2),
-})
-
-
-minetest.register_craftitem("food:bun_mix", {
-	description = "Bun Mix",
-	inventory_image = "food_bun_mix.png",
 })
 
 minetest.register_craftitem("food:bun", {
@@ -46,18 +32,4 @@ minetest.register_craftitem("food:bun", {
 	inventory_image = "food_bun.png",
 	on_use = minetest.item_eat(4),
 	groups={food=2},
-})
-
-minetest.register_craft({
-	type = "cooking",
-	output = "food:bun",
-	recipe = "food:bun_mix",
-})
-
-minetest.register_craft({
-	output = '"food:bun_mix" 5',
-	recipe = {
-        {"food:flour", "food:flour", "bucket:bucket_water"},
-        },
-	replacements = {{"bucket:bucket_water", "bucket:bucket_empty"}}
 })
