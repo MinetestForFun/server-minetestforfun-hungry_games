d17d6f5766d1ebf51799bbef086ca5f6e87193a9
Obani Gemini <ObaniGemini@users.noreply.github.com>
2015-08-08 13:17:24 +0200

    Removed craftitems
    paper, books, lumps(clay, gold, copper, iron), clay brick

--- mods/default/craftitems.lua
+++ mods/default/craftitems.lua
@@ -6,49 +6,14 @@ minetest.register_craftitem("default:stick", {
 	groups = {stick=1},
 })
 
-minetest.register_craftitem("default:paper", {
-	description = "Paper",
-	inventory_image = "default_paper.png",
-})
-
-minetest.register_craftitem("default:book", {
-	description = "Book",
-	inventory_image = "default_book.png",
-})
-
-minetest.register_craftitem("default:coal_lump", {
-	description = "Coal Lump",
-	inventory_image = "default_coal_lump.png",
-})
-
-minetest.register_craftitem("default:iron_lump", {
-	description = "Iron Lump",
-	inventory_image = "default_iron_lump.png",
-})
-
-minetest.register_craftitem("default:copper_lump", {
-	description = "Copper Lump",
-	inventory_image = "default_copper_lump.png",
-})
-
 minetest.register_craftitem("default:mese_crystal", {
 	description = "Mese Crystal",
 	inventory_image = "default_mese_crystal.png",
 })
 
-minetest.register_craftitem("default:gold_lump", {
-	description = "Gold Lump",
-	inventory_image = "default_gold_lump.png",
-})
-
 minetest.register_craftitem("default:diamond", {
 	description = "Diamond",
 	inventory_image = "default_diamond.png",
-})
-
-minetest.register_craftitem("default:clay_lump", {
-	description = "Clay Lump",
-	inventory_image = "default_clay_lump.png",
 })
 
 minetest.register_craftitem("default:steel_ingot", {
@@ -74,11 +39,6 @@ minetest.register_craftitem("default:gold_ingot", {
 minetest.register_craftitem("default:mese_crystal_fragment", {
 	description = "Mese Crystal Fragment",
 	inventory_image = "default_mese_crystal_fragment.png",
-})
-
-minetest.register_craftitem("default:clay_brick", {
-	description = "Clay Brick",
-	inventory_image = "default_clay_brick.png",
 })
 
 minetest.register_craftitem("default:scorched_stuff", {
