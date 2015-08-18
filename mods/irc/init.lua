5892e546f4006c3d8321296baee21def520f532a
Ombridride <Ombridride@users.noreply.github.com>
2015-08-19 00:25:21 +0200

    one path

--- mods/irc/init.lua
+++ mods/irc/init.lua
@@ -18,18 +18,7 @@ ie.package.path =
 		-- For LuaIRC to find its files
 		..modpath.."/?.lua;"
 		..ie.package.path
-		..";/usr/lib/*/lua/5.1/socket/?.so"
-		..";/usr/lib/?/lua/5.1/socket/*.so"
-		..";/usr/lib/x86_64-linux-gnu/liblua5.1-socket.so.2"
 		..";/usr/lib/x86_64-linux-gnu/lua/5.1/core.so"
-		..";/usr/lib/x86_64-linux-gnu/lua/5.1/core.*"
-		..";/usr/lib/x86_64-linux-gnu/lua/5.1/core.?"
-		..";/usr/lib/x86_64-linux-gnu/lua/5.1/mime/core.so"
-		..";/usr/lib/x86_64-linux-gnu/lua/5.1/socket/core.so"
-		..";/usr/lib/x86_64-linux-gnu/lua/5.1/socket/*.so"
-		..";/usr/lib/x86_64-linux-gnu/lua/5.1/socket/?.so"
-		..";/usr/lib/x86_64-linux-gnu/lua/5.1/socket/core.*"
-		..";/usr/lib/x86_64-linux-gnu/lua/5.1/socket/core.?"
 
 -- The build of Lua that Minetest comes with only looks for libraries under
 -- /usr/local/share and /usr/local/lib but LuaSocket is often installed under
