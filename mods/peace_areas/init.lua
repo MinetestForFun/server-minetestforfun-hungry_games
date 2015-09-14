79bf3d9e9d8e93129888437ae9f343480abe0b2a
LeMagnesium <mg.minetest@gmail.com>
2015-09-14 20:45:41 +0200

    Regenerate health/thirst/hunger for players in lobby  - Fix #62

--- mods/peace_areas/init.lua
+++ mods/peace_areas/init.lua
@@ -5,9 +5,9 @@
 
 peace_areas = {areas = {}}
 
-minetest.register_on_punchplayer(function(player, hitter)
+function is_in_peace_area(player)
 	local pos = player:getpos() 
-	if pos == nil then return end
+	if pos == nil then return false end
 	for name, positions in pairs(peace_areas.areas) do
 		local pos1 = positions["pos1"]
 		local pos2 = positions["pos2"]
@@ -24,17 +24,22 @@ minetest.register_on_punchplayer(function(player, hitter)
 		if minp.x < pos.x and pos.x < maxp.x and
 			minp.y < pos.y and pos.y < maxp.y and
 			minp.z < pos.z and pos.z < maxp.z then
-			if hitter:is_player() then
-				minetest.chat_send_player(hitter:get_player_name(), "You cannot punch player " ..
-					player:get_player_name() .. ". They are in area '" .. name .. "'.")
-			end
 			return true
-			--[[
-			Note:
-				This use of the callback register_on_punchplayer may prevent other
-				instance of it to work correctly. Please take note of it.
-			]]--
 		end
+	end
+	return false
+end
+
+minetest.register_on_punchplayer(function(player, hitter)
+	if is_in_peace_area(player) and hitter:is_player() then
+		minetest.chat_send_player(hitter:get_player_name(), "You cannot punch player " ..
+			player:get_player_name() .. ". They are in area '" .. name .. "'.")
+		return true
+		--[[
+		Note:
+			This use of the callback register_on_punchplayer may prevent other
+			instance of it to work correctly. Please take note of it.
+		]]--
 	end
 end)
 
@@ -46,3 +51,17 @@ peace_areas.register_area("lobby", {
 	["pos1"] = {x = 100, y = 30150, z = 135},
 	["pos2"] = {x = -195, y = 29950, z = -135},
 })
+
+
+function regenerate()
+	for name, player in pairs(minetest.get_connected_players()) do
+		if not minetest.get_player_privs(player:get_player_name()).ingame and is_in_peace_area(player) then
+			player:set_hp(20)
+			survival.reset_player_state(player:get_player_name(), "hunger")
+			survival.reset_player_state(player:get_player_name(), "thirst")
+		end
+	end
+	minetest.after(1, regenerate)
+end
+
+regenerate()
