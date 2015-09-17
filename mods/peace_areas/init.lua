632e7d0b42ba810d77fdfad1cea61785730275ad
LeMagnesium <mg.minetest@gmail.com>
2015-09-17 17:28:39 +0200

    [peace_area] Fix crash

--- mods/peace_areas/init.lua
+++ mods/peace_areas/init.lua
@@ -30,10 +30,32 @@ function is_in_peace_area(player)
 	return false
 end
 
+function peace_area(pos)
+	for name, positions in pairs(peace_areas.areas) do
+		local pos1 = positions["pos1"]
+		local pos2 = positions["pos2"]
+		local minp = {
+			x = math.min(pos1.x, pos2.x),
+			y = math.min(pos1.y, pos2.y),
+			z = math.min(pos1.z, pos2.z)
+		}
+		local maxp = {
+			x = math.max(pos1.x, pos2.x),
+			y = math.max(pos1.y, pos2.y),
+			z = math.max(pos1.z, pos2.z)
+		}
+		if minp.x < pos.x and pos.x < maxp.x and
+			minp.y < pos.y and pos.y < maxp.y and
+			minp.z < pos.z and pos.z < maxp.z then
+			return name
+		end
+	end
+end
+
 minetest.register_on_punchplayer(function(player, hitter)
 	if is_in_peace_area(player) and hitter:is_player() then
 		minetest.chat_send_player(hitter:get_player_name(), "You cannot punch player " ..
-			player:get_player_name() .. ". They are in area '" .. name .. "'.")
+			player:get_player_name() .. ". They are in area '" .. peace_area(pos) .. "'.")
 		return true
 		--[[
 		Note:
